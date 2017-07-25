ab6c81b846868717143bee8165c399d916b5984d
RL Brown <rlbrown72@gmail.com>
2017-07-25 13:12:30 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-/* eslint-disable no-console */
-
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import session from 'express-session';
 import mongoose from 'mongoose';
 import routes from './routes';
-
 /**
- * setup dotenv, express
+ * setup dotenv, express, express-session
  */
-
 require('dotenv').config();
 
 const port = process.env.PORT;
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -25,16 +20,14 @@ app.use(
     saveUninitialized: true,
   })
 );
-
 /**
- * connect to mLab with mongoose
+ * connect to dB @ mLab with mongoose
  */
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO, { useMongoClient: true });
-
 /**
  * serve routes, static /public folder, and start server
  */
 app.use(routes);
 app.use(express.static(path.join(__dirname, '../public')));
-app.listen(port, () => console.log(`on ${port}`));
+app.listen(port, () => console.log(`on ${port}`)); // eslint-disable-line
