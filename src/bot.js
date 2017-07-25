fd6a1ff5d2fa080d439a6dfb8e70a4656d6410f1
RL Brown <rlbrown72@gmail.com>
2017-07-25 17:38:57 -0400

    adding bot info and chat-write-bot scope

---
+++ src/bot.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+import slack from 'slack';
+import Token from '../app/tokens';
+
+export default function bot() {
+  // const botTokenPromise = Token.findOne({ access_token: data.access_token }).exec();
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import mongoose from 'mongoose';
 import routes from './routes';
+import bot from './bot';
 /**
  * setup dotenv, express, express-session
  */
@@ -28,6 +29,12 @@ mongoose.connect(process.env.MONGO, { useMongoClient: true });
 /**
  * serve routes, static /public folder, and start server
  */
+// test
+app.post('/events', (req, res) => {
+  const { challenge } = req.body;
+  res.json({ challenge });
+});
+// end test
 app.use(routes);
 app.use(express.static(path.join(__dirname, '../public')));
 app.listen(port, () => console.log(`on ${port}`)); // eslint-disable-line
