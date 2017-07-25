4249f5bf7f29f475f993fa1c28391c972944d8b7
RL Brown <rlbrown72@gmail.com>
2017-07-25 12:28:55 -0400

    separating routes and newssource getter

---
+++ src/news.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default url =>
+  new Promise(
+    resolve => {
+      const newsSources = [];
+      axios
+        .get(url)
+        .then(response => {
+          response.data.sources.forEach(item => {
+            newsSources.push({
+              text: item.name,
+              value: item.id,
+            });
+          });
+          resolve(newsSources);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    reject => {
+      reject({ news: 'source ' });
+    }
+  );
